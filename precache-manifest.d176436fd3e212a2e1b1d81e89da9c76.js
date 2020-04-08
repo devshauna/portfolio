ba74835a1f0a20306bba355ef2e07d098e85a264
sjiang <support@sererra.com>
2020-04-08 14:27:34 -0700

    Updates

---
+++ precache-manifest.d176436fd3e212a2e1b1d81e89da9c76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdbe7f82d3fafbfbd6e6f0a6e289df29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "316e85a9f6c9c6ac9d79",
+    "url": "/static/css/main.cd94acd4.chunk.css"
+  },
+  {
+    "revision": "c894e8a8e01d6a599ce8",
+    "url": "/static/js/2.07a05dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.07a05dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "316e85a9f6c9c6ac9d79",
+    "url": "/static/js/main.4a6b0373.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
